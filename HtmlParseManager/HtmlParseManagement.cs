a2a82177b42b43ff72cdc9d8767cd66f7cfeef2e
Onur Civanoğlu <onurcivanoglu@yandex.com>
2020-07-07 17:43:23 +0300

    Add project files.

---
+++ HtmlParseManager/HtmlParseManagement.cs
@@ -0,0 +1,44 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlParseManager
+{
+    public class HtmlParseManagement
+    {
+        public string xPath { get; set; }
+        public string url { get; set; }
+        public HtmlParseManagement(string xPath, string url)
+        {
+            this.xPath = xPath;
+            this.url = url;
+        }
+        public string GetHtmlText()
+        {
+            try
+            {
+                System.Net.ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
+                var html = url;
+
+                HtmlWeb web = new HtmlWeb();
+
+                var htmlDoc = web.Load(html);
+                xPath = xPath.Replace("\"", "\'");
+                var node = htmlDoc.DocumentNode.SelectSingleNode(xPath); ////*[@id="from-web"]/span                
+                if (node == null)
+                {
+                    return "Not Found";
+                }
+                return node.InnerText;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
---
+++ HtmlParser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlParseManager.HtmlParseManagement parseManagement = new HtmlParseManager.HtmlParseManagement("/html/body/font/font/font/p[4]/table", "https://www.kadifeli.com/vasil/detayliKC.html");
+            var result = parseManagement.GetHtmlText();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
